55850cc47661448ae233da64d488c607428b4ee0
mnshaqir <116859685+mnshaqir@users.noreply.github.com>
2022-12-02 11:22:08 +0800

    Add files via upload

---
+++ Dict/Dict/Dict/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> MyD = new Dictionary<string, int>();
+
+            MyD.Add( "One", 1);
+            MyD.Add( "Two", 2);
+            MyD.Add( "Three", 3);
+            MyD.Add( "Four", 4);
+            MyD.Add( "Five", 5);
+            MyD.Add( "Six", 6);
+            MyD.Add( "Seven", 7);
+            MyD.Add( "Eight", 8);
+
+            foreach (var key in MyD.Keys)
+            {
+                Console.WriteLine(MyD[key]);
+            }
+            Console.WriteLine("=====================");
+            Console.WriteLine("Three");
+            Console.WriteLine("=====================");
+
+            if (MyD.ContainsKey("Ten"))
+                Console.WriteLine("Ten");
+            else
+                Console.WriteLine("There is no Ten");
+
+            Console.WriteLine("=====================");
+
+            MyD.Remove("Four");
+            foreach (var key in MyD.Keys)
+            {
+                Console.WriteLine(MyD[key]);
+            }
+            Console.WriteLine("=====================");
+            Console.WriteLine(MyD.Count);
+
+            Console.ReadKey();
+        }
+    }
+}
